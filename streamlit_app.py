8e07693191c2f1d5a0539ef560338d7a3c1dd2cf
anikets022 <144609344+anikets022@users.noreply.github.com>
2025-03-14 13:13:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -322,7 +322,7 @@ st.markdown(
         background: linear-gradient(270deg, #ff7e5f, #feb47b, #86a8e7, #91eac9);
         background-size: 500% 500%;
         animation: gradientAnimation 6s ease infinite;
-        padding: 5px;
+        padding: 2px;
         border-radius: 8px;
         text-align: center;
         margin-top: 20px;
@@ -339,7 +339,7 @@ st.markdown(
     }
     .footer-content .emoji {
         font-size: 84px;
-        margin-top: 15px;
+        margin-top: 20px;
     }
     </style>
     <div class="footer">
